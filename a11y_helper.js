ac9536d303caebb7984165c8d417f0eb26182311
Gautier MICHELIN <gm@ideesculture.com>
2023-11-22 15:26:36 +0100

    a11y_helper

--- a11y_helper.js
+++ a11y_helper.js
@@ -26,30 +26,30 @@ function activateAccessibilityFunctions() {
     let font_smaller = jQuery('.lab-font-smaller');
     let link_underline = jQuery('.lab-link-underline');
     let reset_all = jQuery('.lab-reset');
-    let body = jQuery('body');
+	let html = jQuery('html');
 	let accessibility_button = jQuery('#accessibility_settings_open_close');
 
-    body.toggleClass('font-readable', Cookies.get('readablefont') === 'yes');
-    body.toggleClass('link-underline', Cookies.get('underline') === 'yes');
+    html.toggleClass('font-readable', Cookies.get('readablefont') === 'yes');
+    html.toggleClass('link-underline', Cookies.get('underline') === 'yes');
 
     let fsCount = 100;
     let cookieFont = Cookies.get('lab-font-size');
 
     if (cookieFont) {
         fsCount = parseInt(cookieFont);
-        if (!body.hasClass('fsize' + fsCount)) {
-            body.addClass('fsize' + fsCount);
+        if (!html.hasClass('fsize' + fsCount)) {
+            html.addClass('fsize' + fsCount);
         }
     } else {
-        body.removeClass('fsize70 fsize80 fsize90 fsize100 fsize110 fsize120 fsize130');
+        html.removeClass('fsize70 fsize80 fsize90 fsize100 fsize110 fsize120 fsize130');
     }
 
     font_larger.click(function(event) {
         event.preventDefault();
         if (fsCount < 130) {
-            body.removeClass('fsize' + fsCount);
+            html.removeClass('fsize' + fsCount);
             fsCount = fsCount + 10;
-            body.addClass('fsize' + fsCount);
+            html.addClass('fsize' + fsCount);
             Cookies.set('lab-font-size', fsCount, { expires: 7, path: cookie_path.cookiePath });
         }
     });
@@ -57,9 +57,9 @@ function activateAccessibilityFunctions() {
     font_smaller.click(function(event) {
         event.preventDefault();
         if (fsCount > 70) {
-            body.removeClass('fsize' + fsCount);
+            html.removeClass('fsize' + fsCount);
             fsCount = fsCount - 10;
-            body.addClass('fsize' + fsCount);
+            html.addClass('fsize' + fsCount);
             Cookies.set('lab-font-size', fsCount, { expires: 7, path: cookie_path.cookiePath });
         }
     });
@@ -67,7 +67,7 @@ function activateAccessibilityFunctions() {
     font_normal.click(function(event) {
         event.preventDefault();
         fsCount = 100;
-        body.removeClass('fsize70 fsize80 fsize90 fsize100 fsize110 fsize120 fsize130 font-readable link-underline');
+        html.removeClass('fsize70 fsize80 fsize90 fsize100 fsize110 fsize120 fsize130 font-readable link-underline');
         Cookies.remove('lab-font-size', { path: cookie_path.cookiePath });
         Cookies.remove('readablefont', { path: cookie_path.cookiePath });
         Cookies.remove('underline', { path: cookie_path.cookiePath });
@@ -76,8 +76,8 @@ function activateAccessibilityFunctions() {
     ReadableFontButton.click(function(event) {
         event.preventDefault();
         Cookies.set('readablefont', 'yes', { expires: 7, path: cookie_path.cookiePath });
-        if (!body.hasClass('font-readable')) {
-            body.addClass('font-readable');
+        if (!html.hasClass('font-readable')) {
+            html.addClass('font-readable');
         }
 
         jQuery(window).trigger('resize');
@@ -86,17 +86,17 @@ function activateAccessibilityFunctions() {
     link_underline.click(function(event) {
         event.preventDefault();
         Cookies.set('underline', 'yes', { expires: 7, path: cookie_path.cookiePath });
-        if (!body.hasClass('link-underline')) {
-            body.addClass('link-underline');
+        if (!html.hasClass('link-underline')) {
+            html.addClass('link-underline');
         }
     });
 	accessibility_button.click(function(event) {
 		console.log('click accessibility_button');
-		body.toggleClass('accessibility-menu-open');
+		html.toggleClass('accessibility-menu-open');
 	});
 
     reset_all.click(function() {
-        body.removeClass('fsize70 fsize80 fsize90 fsize100 fsize110 fsize120 fsize130 font-readable link-underline fontfamily_inter fontfamily_andika fontfamily_fsme fontfamily_tiresias fontfamily_opendyslexic');
+        html.removeClass('fsize70 fsize80 fsize90 fsize100 fsize110 fsize120 fsize130 font-readable link-underline fontfamily_inter fontfamily_andika fontfamily_fsme fontfamily_tiresias fontfamily_opendyslexic');
         fsCount = 100;
         Cookies.remove('lab-font-size', { path: cookie_path.cookiePath });
         Cookies.remove('readablefont', { path: cookie_path.cookiePath });
@@ -109,90 +109,9 @@ function activateAccessibilityFunctions() {
         setFont(this.className);
     });
 
-    body.addClass(Cookies.get('fontfamily'));
+    html.addClass(Cookies.get('fontfamily'));
 
 };
-
-(function($) {
-    "use strict";
-
-    $.fn.countTo = function(options) {
-        options = options || {};
-
-        return $(this).each(function() {
-            // set options for current element
-            let settings = $.extend({}, $.fn.countTo.defaults, {
-                from: $(this).data('from'),
-                to: $(this).data('to'),
-                speed: $(this).data('speed'),
-                refreshInterval: $(this).data('refresh-interval'),
-                decimals: $(this).data('decimals')
-            }, options);
-
-            // how many times to update the value, and how much to increment the value on each update
-            let loops = Math.ceil(settings.speed / settings.refreshInterval),
-                increment = (settings.to - settings.from) / loops;
-
-            // references & variables that will change with each update
-            let self = this,
-                $self = $(this),
-                loopCount = 0,
-                value = settings.from,
-                data = $self.data('countTo') || {};
-
-            $self.data('countTo', data);
-
-            // if an existing interval can be found, clear it first
-            if (data.interval) {
-                clearInterval(data.interval);
-            }
-
-            data.interval = setInterval(updateTimer, settings.refreshInterval);
-
-            // initialize the element with the starting value
-            render(value);
-
-            function updateTimer() {
-                value += increment;
-                loopCount++;
-
-                render(value);
-
-                if (typeof(settings.onUpdate) == 'function') {
-                    settings.onUpdate.call(self, value);
-                }
-
-                if (loopCount >= loops) {
-                    // remove the interval
-                    $self.removeData('countTo');
-                    clearInterval(data.interval);
-                    value = settings.to;
-
-                    if (typeof(settings.onComplete) == 'function') {
-                        settings.onComplete.call(self, value);
-                    }
-                }
-            }
-
-            function render(value) {
-                let formattedValue = settings.formatter.call(self, value, settings);
-                $self.html(formattedValue + "+");
-            }
-        });
-    };
-
-    $.fn.countTo.defaults = {
-        from: 0, // the number the element should start at
-        to: 0, // the number the element should end at
-        speed: 1000, // how long it should take to count between the target numbers
-        refreshInterval: 100, // how often the element should be updated
-        decimals: 0, // the number of decimal places to show
-        formatter: formatter, // handler for formatting the value before rendering
-        onUpdate: null, // callback method for every time the element is updated
-        onComplete: onComplete // callback method for when the element finishes updating
-    };
-
-})(jQuery);
 
 $(document).ready(function() {
 	$("body").prepend('\
